3499f2f37afe003d0c085a9480349b8f57aea701
Aniket Raj <118242466+theaniketraj@users.noreply.github.com>
2023-10-05 23:01:30 +0530

    Add files via upload

---
+++ MeetingView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+struct MeetingView: View
+{
+    var body: some View
+    {
+        VStack
+        {
+            ProgressView(value: 5, total: 15)
+            HStack
+            {
+                VStack(alignment: .leading)
+                {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Space()
+                VStack(alignment: .trailing)
+                {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack
+            {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {})
+                {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+struct MeetingView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        MeetingView()
+    }
+}
